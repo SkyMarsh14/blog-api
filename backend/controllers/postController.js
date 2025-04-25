bc466751a1385c43cc757bcdb192fe07239f4358
Sky Randy <soras1758@gmail.com>
2025-04-26 05:16:58 +0900

    add post methods apis

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,7 +1,61 @@
+import prisma from "../db/prisma.js";
 const postContoller = {
-  allPosts: async (req, res) => {
-    //Get posts from db.
-    res.json({ message: "Welcome to posts route" });
+  get_all: async (req, res) => {
+    const posts = await prisma.post.findMany();
+    res.json({ message: "Welcome to posts route", posts });
+  },
+  get_post: async (req, res) => {
+    const postId = req.params.postId;
+    const post = await prisma.post.findUnique({
+      where: {
+        id: postId,
+      },
+    });
+    res.json({ post });
+  },
+  create_one: async (req, res) => {
+    const { title, content } = req.body;
+    try {
+      const post = await prisma.post.create({
+        data: {
+          title: title,
+          content: content,
+          authorId: req.user.id,
+        },
+      });
+      return res.json({ post });
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+  update: async (req, res) => {
+    const { title, content } = req.body;
+    try {
+      const post = await prisma.post.update({
+        where: {
+          id: req.params.postId,
+        },
+        data: {
+          title,
+          content,
+        },
+      });
+      return res.json({ post });
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const deletedPost = await prisma.post.delete({
+        where: {
+          id: req.params.postId,
+        },
+      });
+      return res.json({ deletedPost });
+    } catch (err) {
+      throw new Error(err);
+    }
   },
 };
 
--- backend/routes/postRouter.js
+++ backend/routes/postRouter.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import postContoller from "./../controllers/postController.js";
 const postRouter = Router();
-postRouter.get("/", postContoller.allPosts);
+postRouter.get("/", postContoller.get_all);
+postRouter.get("/:postId", postContoller.get_post);
+postRouter.post("/", postContoller.create_one);
+postRouter.put("/:postId", postContoller.update);
+postRouter.delete("/:postId", postContoller.delete);
 
 export default postRouter;
