48a3c0b076fc6c93704ec0d965ac5a12860b5ae8
Sky Randy <soras1758@gmail.com>
2025-05-01 14:27:42 +0900

    add login css module

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,20 @@
 import Form from "../components/Form";
+import styles from "../styles/Login.module.css";
 const Login = () => {
   return (
-    <div>
-      <Form />
-    </div>
+    <main>
+      <div className={styles.main_container}>
+        <div className={styles.text_center}>
+          <div>Welcome back</div>
+          <div>Sign in to your account to continue</div>
+        </div>
+        <Form />
+        <div>
+          <p>Don't have an account?</p>
+          <a href="sign-up">Sign up</a>
+        </div>
+      </div>
+    </main>
   );
 };
 export default Login;
