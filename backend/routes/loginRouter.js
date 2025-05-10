93b9830c66fa3d06a145b5014423d33f4a9908a7
Sky Randy <soras1758@gmail.com>
2025-05-10 15:54:13 +0900

    remove logout router as using jwt

--- backend/routes/loginRouter.js
+++ backend/routes/loginRouter.js
@@ -4,13 +4,5 @@ import loginController from "../controllers/loginControllers.js";
 const loginRouter = Router();
 loginRouter.post("/sign-up", loginController.create_user);
 loginRouter.post("/", loginController.validate_login);
-loginRouter.post("/logout", (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.json({ message: "Logout successful" });
-  });
-});
 
 export default loginRouter;
