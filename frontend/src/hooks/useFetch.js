7c24266d1cbe4e7c2c48e671cd0064c7f0806b06
Sky Randy <soras1758@gmail.com>
2025-05-10 15:53:28 +0900

    improve fetch function to include token if found. and remove it when it is expired or invalid

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,19 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import UserContext from "../helper/UserContext";
 import blog_api from "../helper/blog_api";
 const useFetch = (url = "", method = "GET") => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(null);
   const [needsAuth, setNeedsAuth] = useState(null);
+  const [auth, setAuth] = useContext(UserContext);
   url = blog_api + url;
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const response = await fetch(url, { method: method }, { mode: "cors" });
+        const token = localStorage.getItem("token");
+        const options = {
+          method: method,
+          headers: {
+            "Content-Type": "application/json",
+            ...(token && { Authorization: `Bearer ${token}` }),
+          },
+          mode: "cors",
+        };
+        const response = await fetch(url, options);
         if (!response.ok) {
           if (response.status === 401) {
             setNeedsAuth(true);
+            setAuth(false);
+            localStorage.removeItem("token");
             return;
           }
           throw new Error(`Response status: ${response.status}`);
