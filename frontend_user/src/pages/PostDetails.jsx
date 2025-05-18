c4fcc076308b010d325fd46c90f79171d6cd2fba
Sky Randy <soras1758@gmail.com>
2025-05-18 20:02:01 +0900

    add post details page

--- frontend_user/src/pages/PostDetails.jsx
+++ frontend_user/src/pages/PostDetails.jsx
@@ -1,10 +1,43 @@
 import useFetch from "../hooks/useFetch";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styles from "../styles/PostDetails.module.css";
+import formatDate from "../helper/formatDate";
 const PostDetails = () => {
   const { postId } = useParams();
-  const navigate = useNavigate();
   const { data, error, loading, needsAuth } = useFetch(`posts/${postId}`);
-  return <div></div>;
+  return (
+    <div className="main_container">
+      <main>
+        {data && (
+          <>
+            <div className={styles.post_container}>
+              <div className={styles.post_title}>{data.post.title}</div>
+              <div className={styles.post_content}>{data.post.content}</div>
+              <div className={styles.post_createdAt}>
+                {formatDate(data.post.createdAt)}
+              </div>
+            </div>
+          </>
+        )}
+        <CommentInput needsAuth={needsAuth} />
+      </main>
+    </div>
+  );
+};
+
+const CommentInput = ({ needsAuth }) => {
+  if (needsAuth) {
+    return (
+      <div className={styles.input}>
+        <input type="text" placeholder="Login to join this conversation." />
+        <a href="/login">Login</a>
+      </div>
+    );
+  }
+  return (
+    <div className={styles.input}>
+      <input type="text" placeholder="Join the conversation." />
+    </div>
+  );
 };
 export default PostDetails;
