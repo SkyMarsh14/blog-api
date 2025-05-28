d16e08ea69abd4c44d294cb9f9b054f9a86551de
Sky Randy <soras1758@gmail.com>
2025-05-28 10:19:37 +0900

    frontend_admin initial commit

---
+++ frontend_admin/src/App/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import UserContext from "../helper/UserContext";
+import { Outlet } from "react-router-dom";
+const App = () => {
+  const getInitialState = () => {
+    const token = localStorage.getItem("token");
+    return token;
+  };
+  const [auth, setAuth] = useState(getInitialState);
+  return (
+    <UserContext.Provider value={[auth, setAuth]}>
+      <Outlet></Outlet>
+    </UserContext.Provider>
+  );
+};
+export default App;
